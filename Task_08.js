5dfe53d5671237d6cb78cd4d1de5b023ea9a26e9
vanta-nele <a.manaka000@gmail.com>
2021-01-13 16:52:50 +0200

    Completed challenge 8; including conditions taking into account plurality/singularity of hours and minutes

--- Task_08.js
+++ Task_08.js
@@ -2,11 +2,24 @@ function convertNumbersToTime(num) {
 
     var hours = Math.floor(num / 60);
     var minutes = num%60;
-
-        return hours + " hour(s) and " + minutes + " minute(s)"
-    }
     
+
+    if ( hours > 1 && minutes > 1){
+        return hours + " hours and " + minutes + " minutes"
+    }
+    else if(hours > 1 && minutes === 1){
+        return hours + " hours and " + minutes + " minute"
+    }
+    else if(hours === 1 && minutes > 1){
+        return hours + " hour and " + minutes + " minutes"
+    }
+
+    else if(hours === 1 && minutes > 1){
+        return hours + " hour and " + minutes + " minutes"
+    }
+}
     
     console.log(convertNumbersToTime(457));
     console.log(convertNumbersToTime(71));
     console.log(convertNumbersToTime(133));
+    console.log(convertNumbersToTime(121))
