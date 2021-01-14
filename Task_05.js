04741f4c0909ac79b337e9746f8452503e715bb6
vanta-nele <a.manaka000@gmail.com>
2021-01-14 11:10:31 +0200

    Made requested changes to challenges 1, 3, 5, and 8

--- Task_05.js
+++ Task_05.js
@@ -1,6 +1,7 @@
-var length1 = 4; 
-var length2 = 9; 
-var length3 = 12; 
-var semiperimeter = (length1 + length2 + length3)/2;
-var area =  Math.sqrt(semiperimeter*((semiperimeter-length1)*(semiperimeter-length2)*(semiperimeter-length3)));
-console.log(area);
+function areaOfTriangle(sideA, sideB, sideC) {
+    let semiperimeter = (1 / 2) * (sideA + sideB + sideC);
+    let area = Math.sqrt(semiperimeter * (semiperimeter - sideA) * (semiperimeter - sideB) * (semiperimeter - sideC));
+    return area + ' units squared';
+  }
+
+  console.log(areaOfTriangle(4, 9, 12))
--- Task_08.js
+++ Task_08.js
@@ -17,9 +17,25 @@ function convertNumbersToTime(num) {
     else if(hours === 1 && minutes > 1){
         return hours + " hour and " + minutes + " minutes"
     }
+
+    else if(minutes > 1){
+        return minutes + " minutes"
+    }
+    else if(minutes === 1){
+        return minutes + " minute"
+    }
+    else if(hours > 1){
+        return hours + " hours"
+    }
+    else if(hours === 1){
+        return hours + " hour"
+    }
 }
     
     console.log(convertNumbersToTime(457));
     console.log(convertNumbersToTime(71));
     console.log(convertNumbersToTime(133));
-    console.log(convertNumbersToTime(121))
+    console.log(convertNumbersToTime(121));
+    console.log(convertNumbersToTime(60));
+    console.log(convertNumbersToTime(21));
+    console.log(convertNumbersToTime(1));
